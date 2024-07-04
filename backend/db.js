9341404abffece3a335db81df804a950aba296cc
koshal <kk8803647446@gmail.com>
2024-07-04 16:12:47 +0530

    Backend Postgres Connection URL Chnages

--- backend/db.js
+++ backend/db.js
@@ -12,22 +12,27 @@ console.log('DB Port:', process.env.DBPORT);
 console.log('DB Name:', process.env.DATABASE);
 
 
-const pool =new Pool(
-  {
-    // user:  "sa@postgresqlashish",
-    // password: "Hypeteq@2023",
-    // host : "postgresqlashish.postgres.database.azure.com",
-    // port: 5432,
-    // database: "e-commerce(koshal)"
+// const pool =new Pool(
+//   {
+//     // user:  "sa@postgresqlashish",
+//     // password: "Hypeteq@2023",
+//     // host : "postgresqlashish.postgres.database.azure.com",
+//     // port: 5432,
+//     // database: "e-commerce(koshal)"
 
   
 
 
-    user : `${process.env.USER}`,
-    password : `${process.env.PASSWORD}`,
-    host : `${process.env.POSTGRE_HOST}`,
-    port : `${process.env.DBPORT}`,
-    database : `${process.env.DATABASE}`,
-}
-);
+//     user : `${process.env.USER}`,
+//     password : `${process.env.PASSWORD}`,
+//     host : `${process.env.POSTGRE_HOST}`,
+//     port : `${process.env.DBPORT}`,
+//     database : `${process.env.DATABASE}`,
+// }
+// );
+
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL,
+})
+
 export default pool;
